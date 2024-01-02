4a95bc282ba5048d9d46afba6db7e15cc3063c8a
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-02 11:33:32 +0200

    gen static post

--- front-end/js/front.js
+++ front-end/js/front.js
@@ -1,9 +1,43 @@
 'use strict';
 console.log('front.js file was loaded');
 
+const postUrl = 'http://localhost:3000/api/posts';
+
+const postsList = document.getElementById('posts-list');
+
 // parisiusti visus postus
 
+(async () => {
+  const [postsArr, error] = await getDataFetch(postUrl);
+
+  console.log('error ===', error);
+
+  console.log('postsArr ===', postsArr);
+  const elsArr = postsArr.map(makeSinglePost);
+  console.log('elsArr ===', elsArr);
+  postsList.append(...elsArr);
+})();
+
 // sugeneruoti juos htmle
+
+function makeSinglePost(pObj) {
+  const liEl = document.createElement('li');
+  liEl.className = 'col-md-6 col-lg-4';
+  liEl.innerHTML = `
+  <div class="card mb-3">
+    <div class="card-body">
+      <h5 class="card-title">Title</h5>
+      <h6 class="card-subtitle mb-2 text-muted">Date</h6>
+      <p class="card-text">autorius</p>
+      <a href="#" class="btn btn-primary">Read more</a>
+    </div>
+    <div class="card-footer">
+      <p class="lead mb-0">Comments: 6</p>
+    </div>
+  </div>  
+  `;
+  return liEl;
+}
 
 // helper fetch fn
 
